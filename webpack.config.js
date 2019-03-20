227eb1dd569171775ce369d028d45b900758ba3c
za-yn <yuan.z@u.nus.edu>
2019-03-21 00:38:05 +0800

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
+    publicPath: "/webpack/",
     libraryTarget: 'commonjs2',
   },
   module: {
